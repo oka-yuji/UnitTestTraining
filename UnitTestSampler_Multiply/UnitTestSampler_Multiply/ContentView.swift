4c49e0b836ae201a6c58897af35e2425af64b32a
岡優志 <yu.j.0513@gmail.com>
2021-07-23 14:44:39 +0900

    feat: UnitTestの為のサンプルファイルを追加

---
+++ UnitTestSampler_Multiply/UnitTestSampler_Multiply/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitTestSampler_Multiply
+//
+//  Created by 岡優志 on 2021/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UnitTestSampler_Multiply/UnitTestSampler_Multiply/UnitTestSampler_MultiplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestSampler_MultiplyApp.swift
+//  UnitTestSampler_Multiply
+//
+//  Created by 岡優志 on 2021/07/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestSampler_MultiplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
