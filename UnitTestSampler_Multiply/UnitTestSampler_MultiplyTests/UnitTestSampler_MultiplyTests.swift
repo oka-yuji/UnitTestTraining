74e19dc8a0689766cda98863c68ecdadaca6b7c4
岡優志 <yu.j.0513@gmail.com>
2021-07-30 11:02:53 +0900

    test: stubを注入したtestを追加

--- UnitTestSampler_Multiply/UnitTestSampler_MultiplyTests/UnitTestSampler_MultiplyTests.swift
+++ UnitTestSampler_Multiply/UnitTestSampler_MultiplyTests/UnitTestSampler_MultiplyTests.swift
@@ -9,21 +9,28 @@ import XCTest
 @testable import UnitTestSampler_Multiply
 
 class UnitTestSampler_MultiplyTests: XCTestCase {
-    let multiplyModel = MultiplyModel()
-    var multiplyViewModel: MultiplyViewModel!
-    
-    func test_num1とnum2の数字が掛け算されていれば成功() {
-        let num1 = 10
-        let num2 = 5
-        let result = multiplyModel.multiply(num1: num1, num2: num2)
+    func test_num1とnum2の引数が掛け算されていれば成功() {
+        //前処理
+        let multiplyModel = MultiplyModel()
+        //実行
+        let result = multiplyModel.multiply(num1: 10, num2: 5)
+        //検証
         XCTAssertEqual(50, result)
+        //後処理
     }
     
-    func test() {
-        multiplyViewModel.Value1 = ""
-        multiplyViewModel.Value2 = ""
+    struct Stub: MultiplyInterface{
+        func multiply(num1: Int, num2: Int) -> Int {
+            return num1 * num2
+        }
+    }
+    
+    func test_数字以外入力された場合は０を表示() {
+        let multiplyViewModel = MultiplyViewModel(calculate: Stub())
+        multiplyViewModel.Value1 = "a"
+        multiplyViewModel.Value2 = "10"
         multiplyViewModel.multiply()
         let result = multiplyViewModel.multiplyValue
-        XCTAssertEqual("50", result)
+        XCTAssertEqual("0", result)
     }
 }
